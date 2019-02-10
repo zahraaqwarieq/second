fe2cd1c9e70645a99e4ad54a24ef5186411a3101
zahraaqwarieq <47283862+zahraaqwarieq@users.noreply.github.com>
2019-02-10 20:17:24 +0200

    hello

---
+++ sec/src/sec/ss.java
@@ -0,0 +1,12 @@
+package sec;
+
+import javax.swing.JOptionPane;
+
+public class ss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ JOptionPane.showMessageDialog(null, "start");
+	}
+
+}
